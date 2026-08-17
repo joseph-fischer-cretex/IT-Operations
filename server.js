d75ed065eb9ed956c2fce3902dfb5d152a7e56ca
Joseph.Fischer <Joseph.Fischer@cretex.com>
2026-08-17 10:25:52 -0500

    fix error 500

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Serve your static frontend files (like index.html)
 app.use(express.static(path.join(__dirname, 'public')));
